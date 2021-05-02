ddfee01cccf41f913173ac1e4925c0f428895797
Emy Ifeora <emy@meetkumba.com>
2021-05-02 20:50:39 +0100

    change commands

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const volleyball = require('volleyball');
 const path = require('path');
 
