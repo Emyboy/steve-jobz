83f3f9029595082d7852182b40f75b861eff58ad
Emy Ifeora <emy@meetkumba.com>
2021-05-02 20:44:38 +0100

    ..

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express();
+const PORT = process.env.PORT || 8080;
+const volleyball = require('volleyball');
+const path = require('path');
+
+app.use(express.static('build'))
+app.use(volleyball)
+
+app.get('/users', (req, res) => {
+    res.status(200).json([
+        {
+            id: 1,
+            username: "emyboy"
+        }
+    ])
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
