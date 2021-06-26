d25eef1bf1e7feaba78037e061bfc52a37d1fb77
emykumba <emy@meetkumba.com>
2021-06-27 00:33:07 +0100

    added express session

--- app.js
+++ app.js
@@ -7,12 +7,36 @@ const PORT = process.env.PORT;
 const volleyball = require('volleyball');
 const path = require('path');
 const rootRoute = require('./api/routes/root.route');
+const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session);
 
-app.use(express.static('build'))
+new knexSessionStore({
+    knex: require('./api/db/index'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+});
+
+app.use(session({
+    secret: process.env.SECRET,
+    name: 'sid',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 2,
+        sameSite: true,
+        secure: process.env.NODE_ENV === 'production'
+    }
+}));
+
+app.use(express.static('build'));
 app.use(volleyball)
 
 
+app.use('/api/v1', rootRoute);
 app.get('/*', (req, res) => {
+    console.log('SESSION ---', req.session)
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
@@ -20,7 +44,6 @@ app.get('/', (req, res) => {
     res.json({ message: 'it worked' })
 })
 
-app.use('/api/v1', rootRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
